cfd9277a80cea14f253767a8a622e6b3e4de23e4
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-11-22 19:35:28 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ import xbmc
 import xbmcaddon
 
 # Constants
-BROADCAST_IP = "192.168.1.113"  # Replace with the correct broadcast IP for your network
+BROADCAST_IP = "192.168.1.255"  # Replace with the correct broadcast IP for your network
 BROADCAST_PORT = 3074
 
 # Typical XBMC command stuff, grabs system info from info labels.
@@ -13,19 +13,22 @@ def get_system_info():
     ip_address = xbmc.getIPAddress()
     fan_speed = xbmc.getInfoLabel("System.FanSpeed")
     cpu_temp = xbmc.getInfoLabel("System.CPUTemperature")
+    cpu_usage = xbmc.getInfoLabel("System.CPUUsage")
     gpu_temp = xbmc.getInfoLabel("System.GPUTemperature")
+    hdd_temp = xbmc.getInfoLabel("system.hddtemperature")
+    free_space = xbmc.getInfoLabel("system.totalspace")
     
     # Music
     track = xbmc.getInfoLabel("MusicPlayer.Title")
     artist = xbmc.getInfoLabel("MusicPlayer.Artist")
     album = xbmc.getInfoLabel("MusicPlayer.Album")
-    time_during = xbmc.getInfoLabel("MusicPlayer.Time")
-    time_remaining = xbmc.getInfoLabel("MusicPlayer.TimeRemaining")
+    time_during = xbmc.getInfoLabel("Player.Time")
+    time_remaining = xbmc.getInfoLabel("Player.TimeRemaining")
     
     # Video
     title = xbmc.getInfoLabel("VideoPlayer.Title")
-    time_during_video = xbmc.getInfoLabel("VideoPlayer.Time")
-    time_remaining_video = xbmc.getInfoLabel("VideoPlayer.TimeRemaining")
+    time_during_video = xbmc.getInfoLabel("Player.Time")
+    time_remaining_video = xbmc.getInfoLabel("Player.TimeRemaining")
     
     return {
         "bios_version": bios_version,
@@ -33,7 +36,10 @@ def get_system_info():
         "ip_address": ip_address,
         "fan_speed": fan_speed,
         "cpu_temp": cpu_temp,
+        "cpu_usage": cpu_usage,
         "gpu_temp": gpu_temp,
+        "hdd_temp": hdd_temp,
+        "free_space": free_space,
         # Music
         "track": track,
         "artist": artist,
@@ -46,7 +52,7 @@ def get_system_info():
         "time_remaining_video": time_remaining_video
     }
 
-# We love text sanitization, yay!
+# We love text sanitization, yay! - Edit: We hate text sanitization actually, this fucked me up so bad for a bit trying to debug why CPU values just weren't working. Here's an implementation that doesn't break things.
 def sanitize(text):
     try:
         # Remove non-ASCII characters
@@ -71,7 +77,7 @@ def broadcast_system_info():
         system_info = get_system_info()
         sanitized_info = {k: sanitize(v) for k, v in system_info.items()}  # Sanitize all fields
         
-        message = "BIOS: {bios_version}, Free Mem: {free_memory}, IP: {ip_address}, Fan: {fan_speed}, CPU: {cpu_temp}, GPU: {gpu_temp}, Track: {track}".format(**sanitized_info)
+        message = "BIOS: {bios_version}, Free Mem: {free_memory}, IP: {ip_address}, Fan: {fan_speed}, CPU: {cpu_temp}, CPU Usage: {cpu_usage}, GPU: {gpu_temp}, Track: {track}, Title: {title}, HDD: {hdd_temp}, Free Space: {free_space}".format(**sanitized_info)
         
         try:
             sock.sendto(message, (BROADCAST_IP, BROADCAST_PORT))
